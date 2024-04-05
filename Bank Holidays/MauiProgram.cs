5bf6e0cce0d1d20ac0c92dc8cfe5562bb40571a3
Dan Banfield <73112854+Dan-Banfield@users.noreply.github.com>
2024-04-05 17:16:41 +0100

    Change system bar colours on Android.

--- Bank Holidays/MauiProgram.cs
+++ Bank Holidays/MauiProgram.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Extensions.Logging;
+using Microsoft.Maui.LifecycleEvents;
 
 namespace Bank_Holidays
 {
@@ -9,6 +10,25 @@ namespace Bank_Holidays
             var builder = MauiApp.CreateBuilder();
             builder
                 .UseMauiApp<App>()
+                .ConfigureLifecycleEvents(events => 
+                {
+#if ANDROID
+                    events.AddAndroid(android => android.OnCreate((activity, bundle) => SetSystemBarColour(activity)));
+
+                    static void SetSystemBarColour(Android.App.Activity activity)
+                    {
+                        var navigationBarColour = new Android.Graphics.Color(
+                            (byte)28,
+                            (byte)28,
+                            (byte)28,
+                            (byte)255
+                        );
+
+                        activity.Window.SetNavigationBarColor(navigationBarColour);
+                        activity.Window.SetStatusBarColor(navigationBarColour);
+                    }
+#endif
+                })
                 .ConfigureFonts(fonts =>
                 {
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
