6e7760e94ab719fe5aa32722e277664c3ed3d005
Dan Banfield <73112854+Dan-Banfield@users.noreply.github.com>
2024-04-05 16:31:11 +0100

    Add project files.

---
+++ Bank Holidays/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Bank_Holidays
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Bank Holidays/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace Bank_Holidays
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
